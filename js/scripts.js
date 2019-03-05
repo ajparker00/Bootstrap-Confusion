67119b6d20a18a39f0c1d48527f5bcf09091f2cf
Andre <ajparker000@gmail.com>
2019-03-04 22:14:33 -0800

    Grunt Part 1

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Building and deployment of Websites using Grunt and Gulp task runners
+$("#carousel-button").click(function(){
+    if ($("#carousel-button").children("span").hasClass('fa-pause')) 
+        {
+        $("#mycarousel").carousel('pause');
+        $("#carousel-button").children("span").removeClass('fa-pause');
+        $("#carousel-button").children("span").addClass('fa-play');
+    }
+    else if ($("#carousel-button").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carousel-button").children("span").removeClass('fa-play');
+        $("#carousel-button").children("span").addClass('fa-pause'); 
+}
+});
